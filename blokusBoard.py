a1e03b0998ca908ed570a24a950b712240fb64e7
Reid Yesson <reidyesson@gmail.com>
2018-04-25 04:21:05 -0400

    Delete blokusBoard.py

--- blokusBoard.py
+++
@@ -1,8 +0,0 @@
-def make2dList(rows, cols):
-    a=[]
-    for row in range(rows): a += [[0]*cols]
-    return a
-
-class Board(object):
-	def __init__(self, boardSize):
-		board = made2dList(boardSize, boardSize)
